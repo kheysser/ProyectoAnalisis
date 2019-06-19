89e779362205c372ee2898b0c672a9a5bb220114
kheysser <kheysser@outlook.com>
2019-06-19 01:50:46 -0600

    Add files via upload

---
+++ Proyecto/src/Personas/Productor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personas;
+
+/**
+ *
+ * @author kheysser
+ */
+public class Productor {
+    String Ingreso_producto;
+    int Ingreso_valor_esperado;
+    
+    public void ingresar_producto(){
+        
+    }
+}
---
+++ Proyecto/src/Personas/Vendedor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personas;
+
+/**
+ *
+ * @author kheysser
+ */
+public class Vendedor {
+    int ingreso_ventas;
+    int ingreso_extras;
+    int ingreso_sobrantes;
+    String ingreso_nombreventas;
+    String ingreso_nombreextras;
+    String ingreso_nombresobrantes;
+    
+    public void Agregar_ventas(){
+        
+    }
+    
+    public void Agregar_extras(){
+        
+    }
+    
+    public void Agregar_sobrantes(){
+        
+    }
+}
